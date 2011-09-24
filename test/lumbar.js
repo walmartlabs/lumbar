c6aa2931c3e8903f34db102b11c133c066d76aac
kpdecker <kpdecker@gmail.com>
2011-09-23 23:40:08 -0500

    Make test handler more generic

--- test/lumbar.js
+++ test/lumbar.js
@@ -1,14 +1,30 @@
 var assert = require('assert'),
     fs = require('fs'),
+    glob = require('glob'),
     lumbar = require('../lib/lumbar'),
     wrench = require('wrench');
 
-function runTest(callback) {
+function runTest(configFile, expectedDir, callback) {
   var outdir = '/tmp/lumbar-test-' + Date.now() + Math.random();
-  console.log('Creating test directory: ' + outdir);
+  console.log('Creating test directory ' + outdir + ' for ' + configFile);
   fs.mkdirSync(outdir, 0755);
 
-  callback(outdir, function() {
+  var arise = lumbar.init(configFile, {outdir: outdir});
+  arise.build(undefined, function(err, status) {
+    if (err) {
+      throw err;
+    }
+
+    var generatedFiles = glob.globSync(outdir + '/**/*.js').map(function(fileName) { return fileName.substring(outdir.length); }),
+        expectedFiles = glob.globSync(expectedDir + '/**/*.js').map(function(fileName) { return fileName.substring(expectedDir.length); });
+    assert.deepEqual(generatedFiles, expectedFiles, configFile + ': file list matches');
+
+    generatedFiles.forEach(function(fileName) {
+      var generatedContent = fs.readFileSync(outdir + fileName, 'utf8'),
+          expectedContent = fs.readFileSync(expectedDir + fileName, 'utf8');
+      assert.equal(generatedContent, expectedContent, configFile + ':' + fileName + ': content matches');
+    });
+
     // Cleanup
     wrench.rmdirSyncRecursive(outdir);
   });
