84f59ea33867b9398e0ca13d2d2427f864a46b5d
kpdecker <kpdecker@gmail.com>
2011-12-22 13:13:27 -0600

    File fileList tests

--- test/file-util.js
+++ test/file-util.js
@@ -15,3 +15,49 @@ exports['resolve-path'] = function() {
   assert.equal(fu.resolvePath('c:\\foo'), 'c:\\foo');
   assert.equal(fu.resolvePath('c:/foo'), 'c:/foo');
 };
+
+exports['file-list-file'] = function(done) {
+  fu.fileList('test/file-util.js', function(err, files) {
+    if (err) {
+      throw err;
+    }
+
+    assert.equal(files.length, 1);
+    assert.equal(files[0], 'test/file-util.js');
+    done();
+  });
+};
+
+exports['file-list-file-lookup'] = function(done) {
+  fu.lookupPath('test');
+  fu.fileList('file-util.js', function(err, files) {
+    if (err) {
+      throw err;
+    }
+
+    assert.equal(files.length, 1);
+    assert.equal(files[0], 'test/file-util.js');
+    done();
+  });
+};
+
+exports['file-list-file-no-filter'] = function(done) {
+  fu.fileList('test/file-util.js', /notjason/, function(err, files) {
+    if (err) {
+      throw err;
+    }
+
+    assert.equal(files.length, 1);
+    assert.equal(files[0], 'test/file-util.js');
+    done();
+  });
+};
+
+exports['file-list-file-not-found'] = function(done) {
+  fu.fileList('test/file-util.js.foo', function(err, files) {
+    assert.equal(files, undefined);
+    assert.equal(err.code, 'ENOENT');
+
+    done();
+  });
+};
