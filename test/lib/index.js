8791e0b4cf73c2084a0ea50654e7d58d1c7a6aa6
kpdecker <kpdecker@gmail.com>
2011-12-30 19:39:39 -0600

    Move lumbar test runTest into lib method

--- test/lib/index.js
+++ test/lib/index.js
@@ -1,6 +1,7 @@
 var assert = require('assert'),
     fs = require('fs'),
     glob = require('glob'),
+    lumbar = require('../../lib/lumbar'),
     path = require('path'),
     wrench = require('wrench');
 
@@ -27,3 +28,45 @@ exports.assertExpected = function(outdir, expectedDir, configFile) {
     assert.equal(generatedContent, expectedContent, configFile + ':' + fileName + ': content matches');
   });
 };
+
+exports.runTest = function(configFile, expectedDir, options) {
+  return function(done) {
+    var outdir = exports.testDir('lumbar', configFile);
+    this.title += ' ' + outdir;
+
+    options = options || {};
+    options.outdir = outdir;
+
+    var expectedFiles = glob.globSync(expectedDir + '/**/*.{css,js}').map(function(fileName) {
+          return fileName.substring(expectedDir.length);
+        }).sort(),
+        seenFiles = [];
+
+    var arise = lumbar.init(configFile, options || {outdir: outdir});
+    arise.build(undefined, function(err, status) {
+      if (err) {
+        throw err;
+      }
+
+      var statusFile = status.fileName.substring(outdir.length);
+      if (!expectedFiles.some(function(fileName) { return statusFile === fileName; })) {
+        assert.fail(undefined, status.fileName, configFile + ':' + statusFile + ': missing from expected list');
+      } else {
+        seenFiles.push(statusFile);
+      }
+      if (seenFiles.length < expectedFiles.length) {
+        return;
+      }
+
+      exports.assertExpected(outdir, expectedDir, configFile);
+
+      seenFiles = seenFiles.sort();
+      assert.deepEqual(seenFiles, expectedFiles, 'seen file list matches');
+
+      // Cleanup (Do cleanup here so the files remain for the failure case)
+      wrench.rmdirSyncRecursive(outdir);
+
+      done();
+    });
+  };
+}
--- test/lumbar.js
+++ test/lumbar.js
@@ -1,77 +1,30 @@
-var assert = require('assert'),
-    fs = require('fs'),
-    glob = require('glob'),
-    lib = require('./lib'),
-    lumbar = require('../lib/lumbar'),
-    wrench = require('wrench');
+var lib = require('./lib');
 
-function runTest(configFile, expectedDir, options) {
-  return function(done) {
-    var outdir = lib.testDir('lumbar', configFile);
-    this.title += ' ' + outdir;
+exports['single-file'] = lib.runTest('test/artifacts/single-file.json', 'test/expected/single-file');
+exports['single-dir'] = lib.runTest('test/artifacts/single-directory.json', 'test/expected/js-dir');
+exports['multiple-files'] = lib.runTest('test/artifacts/multiple-files.json', 'test/expected/js-dir');
+exports['file-modules'] = lib.runTest('test/artifacts/file-modules.json', 'test/expected/file-modules');
+exports['multiple-platforms'] = lib.runTest('test/artifacts/multiple-platforms.json', 'test/expected/multiple-platforms');
+exports['multiple-packages'] = lib.runTest('test/artifacts/multiple-packages.json', 'test/expected/multiple-packages');
 
-    options = options || {};
-    options.outdir = outdir;
-
-    var expectedFiles = glob.globSync(expectedDir + '/**/*.{css,js}').map(function(fileName) {
-          return fileName.substring(expectedDir.length);
-        }).sort(),
-        seenFiles = [];
-
-    var arise = lumbar.init(configFile, options || {outdir: outdir});
-    arise.build(undefined, function(err, status) {
-      if (err) {
-        throw err;
-      }
-
-      var statusFile = status.fileName.substring(outdir.length);
-      if (!expectedFiles.some(function(fileName) { return statusFile === fileName; })) {
-        assert.fail(undefined, status.fileName, configFile + ':' + statusFile + ': missing from expected list');
-      } else {
-        seenFiles.push(statusFile);
-      }
-      if (seenFiles.length < expectedFiles.length) {
-        return;
-      }
-
-      lib.assertExpected(outdir, expectedDir, configFile);
-
-      seenFiles = seenFiles.sort();
-      assert.deepEqual(seenFiles, expectedFiles, 'seen file list matches');
-
-      // Cleanup (Do cleanup here so the files remain for the failure case)
-      wrench.rmdirSyncRecursive(outdir);
-
-      done();
-    });
-  };
-}
-
-exports['single-file'] = runTest('test/artifacts/single-file.json', 'test/expected/single-file');
-exports['single-dir'] = runTest('test/artifacts/single-directory.json', 'test/expected/js-dir');
-exports['multiple-files'] = runTest('test/artifacts/multiple-files.json', 'test/expected/js-dir');
-exports['file-modules'] = runTest('test/artifacts/file-modules.json', 'test/expected/file-modules');
-exports['multiple-platforms'] = runTest('test/artifacts/multiple-platforms.json', 'test/expected/multiple-platforms');
-exports['multiple-packages'] = runTest('test/artifacts/multiple-packages.json', 'test/expected/multiple-packages');
-
-exports['module-map'] = runTest('test/artifacts/module-map.json', 'test/expected/module-map');
-exports['router'] = runTest('test/artifacts/router.json', 'test/expected/router');
-exports['template'] = runTest('test/artifacts/template.json', 'test/expected/template');
+exports['module-map'] = lib.runTest('test/artifacts/module-map.json', 'test/expected/module-map');
+exports['router'] = lib.runTest('test/artifacts/router.json', 'test/expected/router');
+exports['template'] = lib.runTest('test/artifacts/template.json', 'test/expected/template');
 
 // TODO : Templates that are dependent on the platform
 // TODO : Error handling for Missing template cache definitions
 // TODO : Test multiple template references in the same file
 
-exports['dev-config'] = runTest('test/artifacts/package-config.json', 'test/expected/dev-config', {packageConfigFile: 'config/dev.json'});
-exports['production-config'] = runTest('test/artifacts/package-config.json', 'test/expected/production-config', {packageConfigFile: 'config/production.json'});
+exports['dev-config'] = lib.runTest('test/artifacts/package-config.json', 'test/expected/dev-config', {packageConfigFile: 'config/dev.json'});
+exports['production-config'] = lib.runTest('test/artifacts/package-config.json', 'test/expected/production-config', {packageConfigFile: 'config/production.json'});
 
-exports['scope-resource'] = runTest('test/artifacts/scope-resource.json', 'test/expected/scope-resource');
-exports['scope-none'] = runTest('test/artifacts/scope-none.json', 'test/expected/scope-none');
-exports['scope-globals'] = runTest('test/artifacts/scope-globals.json', 'test/expected/scope-globals');
-exports['application-namespace'] = runTest('test/artifacts/application-namespace.json', 'test/expected/application-namespace', {packageConfigFile: 'config/dev.json'});
+exports['scope-resource'] = lib.runTest('test/artifacts/scope-resource.json', 'test/expected/scope-resource');
+exports['scope-none'] = lib.runTest('test/artifacts/scope-none.json', 'test/expected/scope-none');
+exports['scope-globals'] = lib.runTest('test/artifacts/scope-globals.json', 'test/expected/scope-globals');
+exports['application-namespace'] = lib.runTest('test/artifacts/application-namespace.json', 'test/expected/application-namespace', {packageConfigFile: 'config/dev.json'});
 
-exports['styles'] = runTest('test/artifacts/styles.json', 'test/expected/styles');
-exports['stylus'] = runTest('test/artifacts/stylus.json', 'test/expected/stylus');
-exports['inline-styles'] = runTest('test/artifacts/inline-styles.json', 'test/expected/inline-styles');
+exports['styles'] = lib.runTest('test/artifacts/styles.json', 'test/expected/styles');
+exports['stylus'] = lib.runTest('test/artifacts/stylus.json', 'test/expected/stylus');
+exports['inline-styles'] = lib.runTest('test/artifacts/inline-styles.json', 'test/expected/inline-styles');
 
 // TODO : Test file not found and other cases
