473b35efb4552842c391282c9747fea6f2eed2e8
Joe Hudson <joehud@gmail.com>
2012-01-09 17:36:17 -0500

    plugins can be defined in lumbar.json file

---
+++ test/artifacts/json-plugins-test-plugin.js
@@ -0,0 +1,17 @@
+var fu = require('../../lib/fileUtil');
+
+module.exports = function(options) {
+  return {
+    mode: 'foo',
+    
+    module: function(context, next, complete) {
+      next(function(err) {
+        var outdir = context.options.outdir;
+        var fileName = options.fileName;
+        fileName = outdir += '/' + fileName;
+        var content = options.content;
+        fu.writeFile(fileName, content, complete);
+      });
+    }
+  }
+}
---
+++ test/expected/json-plugin/home.js
@@ -0,0 +1,8 @@
+var home;
+home = (function() {
+var module = {exports: {}};
+var exports = module.exports;
+var Home = true;
+;;
+return module.exports;
+}).call(this);
