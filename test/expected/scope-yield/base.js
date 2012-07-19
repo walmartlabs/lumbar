de5d8fea004a2740757ce3fde3a5f93c9ce9a856
Ryan Eastridge <ryan@syntacticx.com>
2012-07-19 12:10:59 -0700

    move template loading for scope and template plugins into fileUtil, add scope-yield test

---
+++ test/expected/scope-yield/base.js
@@ -0,0 +1,13 @@
+var base;
+base = (function() {
+  var key = 'value';
+  var module = {exports: {}};
+  var exports = module.exports;
+  var allYour = true;
+
+exports.belongToUs = function() {
+};
+;;
+
+  return module.exports;
+}).call(this);
