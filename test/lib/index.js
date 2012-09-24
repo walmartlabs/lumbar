cf4e562b66fccd6964b608fad735a8cf6e7cbf5c
kpdecker <kpdecker@gmail.com>
2012-09-24 07:52:40 -0500

    Include options on test context

--- test/lib/index.js
+++ test/lib/index.js
@@ -107,6 +107,7 @@ exports.mixinExec = function(module, mixins, config, callback) {
 
   config = Config.create(_.defaults({modules: {module: module}}, config));
   var context = new Context({module: module}, config, plugin, new Mixins({mixins: mixins}));
+  context.options = {};
 
   context.mixins.initialize(context, function(err) {
     if (err) {
