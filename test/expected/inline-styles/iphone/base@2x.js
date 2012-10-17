bf8eda79c578eda003223ad383a8d9efe9eb762f
kpdecker <kpdecker@gmail.com>
2012-10-16 21:13:59 -0500

    Remove scope plugin from inline-styles test

--- test/expected/inline-styles/iphone/base@2x.js
+++ test/expected/inline-styles/iphone/base@2x.js
@@ -1,6 +1,3 @@
-Application['base'] = (function() {
-var module = {exports: {}};
-var exports = module.exports;
 var allYour = true;
 
 exports.belongToUs = function() {
@@ -9,5 +6,3 @@ exports.belongToUs = function() {
 /* lumbar module map */
 module.exports.moduleMap({"modules":{"base":{"js":[{"href":"base.js","maxRatio":1.5},{"href":"base@2x.js","minRatio":1.5}]}},"routes":{}});
 Application.loader.loadInlineCSS("th {\n  font-weight: normal;\n}\nbody {\n  font: 0.9em 'comic sans ms';\n  background: #fff;\n}\n.header-button.show-search {\n  background-image: url(\"images/barrowLoRez@2x.png\");\n}\n");
-return module.exports;
-}).call(this);
