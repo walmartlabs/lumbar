b419691256560d52c881be35c740092c078bcece
kpdecker <kpdecker@gmail.com>
2012-10-16 20:46:30 -0500

    Remove scope plugin from multiple package test

--- test/expected/multiple-packages/iphone/native.js
+++ test/expected/multiple-packages/iphone/native.js
@@ -1,7 +1,3 @@
-var base;
-base = (function() {
-var module = {exports: {}};
-var exports = module.exports;
 var allYour = true;
 
 exports.belongToUs = function() {
@@ -9,13 +5,4 @@ exports.belongToUs = function() {
 ;;
 var iphone = true;
 ;;
-return module.exports;
-}).call(this);
-var home;
-home = (function() {
-var module = {exports: {}};
-var exports = module.exports;
 var Home = true;
-;;
-return module.exports;
-}).call(this);
