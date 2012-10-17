745e13ac33e8d536b362ace145f64304c2711762
kpdecker <kpdecker@gmail.com>
2012-10-16 21:03:59 -0500

    Remove scope plugin from more tests

--- test/expected/js-dir/base.js
+++ test/expected/js-dir/base.js
@@ -1,7 +1,3 @@
-var base;
-base = (function() {
-var module = {exports: {}};
-var exports = module.exports;
 var allYour = true;
 
 exports.belongToUs = function() {
@@ -12,6 +8,3 @@ var Home = true;
 var iphone = true;
 ;;
 var web = true;
-;;
-return module.exports;
-}).call(this);
--- test/expected/mixin/home.js
+++ test/expected/mixin/home.js
@@ -1,10 +1,3 @@
-var home;
-home = (function() {
-var module = {exports: {}};
-var exports = module.exports;
 var global = true;
 ;;
 var mixinBase = true;
-;;
-return module.exports;
-}).call(this);
