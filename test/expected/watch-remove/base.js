c8d6b46a2f42b640f79225c3067aa72bafeb9765
kpdecker <kpdecker@gmail.com>
2012-10-16 21:44:52 -0500

    Update watch test expectations

--- test/expected/watch-remove/base.js
+++ test/expected/watch-remove/base.js
@@ -1,7 +1,3 @@
-var base;
-base = (function() {
-var module = {exports: {}};
-var exports = module.exports;
 var allYour = true;
 
 exports.belongToUs = function() {
@@ -10,6 +6,3 @@ exports.belongToUs = function() {
 var iphone = true;
 ;;
 var web = true;
-;;
-return module.exports;
-}).call(this);
